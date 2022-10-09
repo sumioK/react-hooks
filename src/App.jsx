c6de146c11d12f9602669ca09cecab4b7fa37a1a
Sumio K <tap.miwo@gmail.com>
2022-10-09 15:09:04 +0900

    custom hooks complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useEffect, useState, useContext, useRef, useReducer } from 'react'
+import { useEffect, useState, useContext, useRef, useReducer, useMemo, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import ShinCodeContext from './main';
+import SomeChild from './SomeChild';
+import useLocalStorage from './useLocalStorage';
 
 const reducer = (state, action) => {
   switch(action.type){
@@ -33,7 +35,42 @@ function App() {
     console.log(ref.current.value)
     console.log(ref.current.offsetHeight)
 
-  }
+  };
+
+  //useMemo
+  const [count01, setCount01] = useState(0);
+  const [count02, setCount02] = useState(0);
+
+  // const square = () =>{
+  //   let i = 0;
+  //   while(i < 2000000000){
+  //     i ++ ;
+  //   }
+  //   return count02 * count02;
+  // }
+
+  const square = useMemo(() =>{
+    let i = 0;
+    while(i < 2000000000){
+      i ++ ;
+    }
+    return count02 * count02;
+  },[count02]);
+
+  //useCallback 関数のメモ化
+  const [counter, setCounter] = useState(0);
+
+  // const showCount = () => {
+  //   alert('これは重い処理です。')
+  // }
+
+  const showCount = useCallback(() => {
+    alert('これは重い処理です。')
+  },[counter]);
+
+//カスタムフック
+const [age, setAge] =useLocalStorage("age", 26);
+
   return (
   
   <div className="App">
@@ -59,6 +96,22 @@ function App() {
     <button onClick={() => dispatch({ type: "increment"})}>+</button>
     <button onClick={() => dispatch({ type: "decrement"})}>-</button>
 
+    <hr />
+    <h1>useMemo</h1>
+    <div>カウント1:{count01}</div>
+    <div>カウント2:{count02}</div>
+    <div>結果:{square}</div>
+    <button onClick={() => setCount01(count01 + 1)}>+</button>
+    <button onClick={() => setCount02(count02 + 1)}>+</button>
+
+    <hr />
+    <h1>useCallback</h1>
+    <SomeChild showCount={showCount} />
+
+    <hr />
+    <h1>カスタムフック</h1>
+    <p>{age}</p>
+    <button onClick={() => setAge(80)}>年齢をセット</button>
   </div>
   );
 }
